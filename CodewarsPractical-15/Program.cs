13a80a05ee6703341541f57a87a3f1afddc89ef7
FBR <ozturkridvan1@gmail.com>
2026-09-20 17:58:35 +0300

    Add project files.

---
+++ CodewarsPractical-15/Program.cs
@@ -0,0 +1,25 @@
+﻿static int FindEvenIndex(int[] arr)
+{
+    int totalSum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        totalSum += arr[i];
+    }
+
+    int leftSum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int rightSum = totalSum - leftSum - arr[i];
+
+        if (leftSum == rightSum)
+        {
+            return i;
+        }
+
+        leftSum += arr[i];
+    }
+
+    return -1;
+}
